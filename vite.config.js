f982b3d8afa80877df028eadb1cf5ced20a4e671
山海鲸 <shanhaisilu@foxmail.com>
2023-02-21 10:39:14 +0800

    尝试使用githubPages托管站点

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "./",
+  build: {
+    outDir: "root"
+  },
   server: {
     port: 9999,
     open: true
